ec249e17d62ae2b1e4264e8ff064a59c41076688
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2023-11-11 13:22:56 +0200

    discord rpc thingy

--- lua/profiles/mrcatnerd/plugins/xtras/hacker.lua
+++ lua/profiles/mrcatnerd/plugins/xtras/hacker.lua
@@ -8,18 +8,16 @@ return {
         "HackFollowAuto",
     },
     config = function()
-        vim.schedule(function()
-            require("hacker").setup {
-                -- content = [[ Code want to show.... ]],   -- The code snippet that show when typing
-                -- filetype = "lua",                        -- filetype of code snippet
-                speed = { -- characters insert each time, random from min -> max
-                    min = 2,
-                    max = 10,
-                },
-                -- note: is_popup is very bugged
-                is_popup = false, -- show random float window when typing
-                popup_after = 5,
-            }
-        end)
+        require("hacker").setup {
+            -- content = [[ Code want to show.... ]],   -- The code snippet that show when typing
+            -- filetype = "lua",                        -- filetype of code snippet
+            speed = { -- characters insert each time, random from min -> max
+                min = 2,
+                max = 10,
+            },
+            -- note: is_popup is very bugged
+            is_popup = false, -- show random float window when typing
+            popup_after = 5,
+        }
     end
 }
