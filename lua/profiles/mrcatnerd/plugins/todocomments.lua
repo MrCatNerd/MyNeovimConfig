abd89d03e8006cf66104f34c290c213a26b55e05
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2023-12-04 15:15:50 +0200

    Added lazy loading to todo-comments.nvim, Closes #4

--- lua/profiles/mrcatnerd/plugins/todocomments.lua
+++ lua/profiles/mrcatnerd/plugins/todocomments.lua
@@ -1,16 +1,16 @@
 return {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = {
-        "TodoLocList",
-        "TodoQuickFix",
-        "TodoTrouble",
-        "TodoTelescope",
-    },
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    }
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = "BufReadPost",
+	cmd = {
+		"TodoLocList",
+		"TodoQuickFix",
+		"TodoTrouble",
+		"TodoTelescope",
+	},
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
 }
-
