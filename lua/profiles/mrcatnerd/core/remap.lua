96158fdb487fd645f22ac6aa6e55b1899fcb7274
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-01-29 19:59:50 +0200

    feat: =ap mark remap

--- lua/profiles/mrcatnerd/core/remap.lua
+++ lua/profiles/mrcatnerd/core/remap.lua
@@ -12,6 +12,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "=ap", "mz=ap`z", { desc = "Auto ident paragraph (no lsp) and stay on same position" })
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste motion (p) to void register" })
 
