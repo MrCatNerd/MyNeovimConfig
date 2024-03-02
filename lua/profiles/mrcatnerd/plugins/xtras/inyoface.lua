48ad0bb2836c9a7caf0b85fb863addfb2976120a
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-03-02 14:35:03 +0200

    quick IN YO FACE fix

--- lua/profiles/mrcatnerd/plugins/xtras/inyoface.lua
+++ lua/profiles/mrcatnerd/plugins/xtras/inyoface.lua
@@ -1,10 +1,15 @@
 return {
 	"tjdevries/vim-inyoface",
+	cmd = { "InYoFaceToggle" },
 	keys = {
 		{
 			"<leader>la",
 			"<Plug>(InYoFace_Toggle)<CR>",
 			desc = "IN YO FACE",
+			silent = true,
 		},
 	},
+	config = function()
+		vim.api.nvim_create_user_command("InYoFaceToggle", "silent call inyoface#toggle_comments()", {})
+	end,
 }
