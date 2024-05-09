45a5c0312d9c8ac087cd1a363e76a91d12dd3eb3
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-05-10 00:57:58 +0300

    Added stuff after git almost killing me

--- lua/profiles/mrcatnerd/plugins/git.lua
+++ lua/profiles/mrcatnerd/plugins/git.lua
@@ -38,5 +38,16 @@ return {
 		},
 	},
 	opts = {},
-	config = true,
+	config = function(_, opts)
+		require("neogit").setup(opts)
+
+		local group = vim.api.nvim_create_augroup("MyCustomNeogitEvents", { clear = true })
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "NeogitPushComplete",
+			group = group,
+			callback = function()
+				require("notify").notify("Your code is now destoryed!", "info")
+			end,
+		})
+	end,
 }
