d43b5a03910cbcb5edc463084126702415374a46
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-02-07 00:51:27 +0200

    clang-format off snippert for c/c++
    yes

--- lua/profiles/mrcatnerd/plugins/lsp/filetypes/c.lua
+++ lua/profiles/mrcatnerd/plugins/lsp/filetypes/c.lua
@@ -1,4 +1,4 @@
-local ls = require("luasnip")
+local ls = require "luasnip"
 
 local s = ls.s
 
@@ -7,7 +7,8 @@ local fmt = require("luasnip.extras.fmt").fmt
 local i, t, c = ls.insert_node, ls.text_node, ls.choice_node
 
 ls.add_snippets("c", {
-	ls.parser.parse_snippet("->", "$1->$0"),
-	ls.parser.parse_snippet("vp", "void*$0"),
-	ls.parser.parse_snippet("vpc", "(void*)$0"),
+    ls.parser.parse_snippet("->", "$1->$0"),
+    ls.parser.parse_snippet("vp", "void*$0"),
+    ls.parser.parse_snippet("vpc", "(void*)$0"),
+    ls.parser.parse_snippet("clang-ignore", "// clang-format off\n$0\n// clang-format on"),
 })
--- lua/profiles/mrcatnerd/plugins/lsp/filetypes/cpp.lua
+++ lua/profiles/mrcatnerd/plugins/lsp/filetypes/cpp.lua
@@ -1,5 +1,6 @@
-local ls = require("luasnip")
+local ls = require "luasnip"
 
 ls.add_snippets("cpp", {
-	ls.parser.parse_snippet("->", "$1->$0"),
+    ls.parser.parse_snippet("->", "$1->$0"),
+    ls.parser.parse_snippet("clang-ignore", "// clang-format off\n$0\n// clang-format on"),
 })
