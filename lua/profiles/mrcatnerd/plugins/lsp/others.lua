016a764ad47274586b962feebe1f23bbaddcd80e
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-03-14 18:47:28 +0200

    updated deprecated keyword crates.nvim + added stable tag to it

--- lua/profiles/mrcatnerd/plugins/lsp/others.lua
+++ lua/profiles/mrcatnerd/plugins/lsp/others.lua
@@ -1,23 +1,22 @@
 return {
-	{
-		"saecki/crates.nvim",
-		event = "BufReadPost Cargo.toml",
-		dependencies = "nvim-lua/plenary.nvim",
-		config = function()
-			require("crates").setup({
-				src = {
-					cmp = {
-						enabled = true,
-					},
-				},
-			})
-		end,
-	},
-	{
-		"j-hui/fidget.nvim",
-		event = "LspAttach",
-		config = function()
-			require("fidget").setup()
-		end,
-	},
+    {
+        "saecki/crates.nvim",
+        tag = "stable",
+        event = "BufReadPost Cargo.toml",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = function()
+            require("crates").setup {
+                completion = {
+                    cmp = {
+                        enabled = true,
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "j-hui/fidget.nvim",
+        event = "LspAttach",
+        config = function() require("fidget").setup() end,
+    },
 }
