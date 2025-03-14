29bf97dfd6e5d90f5e90e14f781983f55bb32e18
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-03-14 16:58:28 +0200

    changed surround branch to main cuz i wanted to

--- lua/profiles/mrcatnerd/plugins/surround.lua
+++ lua/profiles/mrcatnerd/plugins/surround.lua
@@ -1,6 +1,6 @@
 return {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "main", -- use `main` branch for the latest features
     keys = {
         { "cs", desc = "Change surrounding" },
         { "ds", desc = "Delete surrounding" },
