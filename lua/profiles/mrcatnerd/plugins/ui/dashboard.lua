9333c225b7a009cbd59d93d4d16406e2f26fed8a
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-06-16 21:23:27 +0300

    fixed: dashboard not showing up on startup
    i just removed the defer thingy

--- lua/profiles/mrcatnerd/plugins/ui/dashboard.lua
+++ lua/profiles/mrcatnerd/plugins/ui/dashboard.lua
@@ -66,7 +66,6 @@ local text_header = {}
 
 return {
     "nvimdev/dashboard-nvim",
-    -- event = "VimEnter", -- default but UIEnter is faster
     event = "UIEnter",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
@@ -77,92 +76,90 @@ return {
         "DashboardUpdateFooter",
     },
     config = function()
-        vim.defer_fn(function()
-            require("dashboard").setup {
-                theme = "doom",
-                config = {
-                    --[[ week_header = {
+        require("dashboard").setup {
+            theme = "doom",
+            config = {
+                --[[ week_header = {
 						enable = true, --boolean use a week header
 					}, ]]
 
-                    -- comment week_header to enable this cat
-                    header = merge_tables(nvim_header, cat_header, 3),
-                    -- header = cat_header,
+                -- comment week_header to enable this cat
+                header = merge_tables(nvim_header, cat_header, 3),
+                -- header = cat_header,
 
-                    center = {
-                        {
-                            icon = "󰦛 ",
-                            icon_hl = "Title",
-                            desc = "Restore Session",
-                            desc_hl = "String",
-                            key = "ss",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = "lua require('persistence').load()",
-                        },
-                        {
-                            icon = " ",
-                            icon_hl = "Title",
-                            desc = "New File",
-                            desc_hl = "String",
-                            key = "n",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = ":silent enew",
-                        },
-                        {
-                            icon = "󰂠 ",
-                            icon_hl = "Title",
-                            desc = "Lazy",
-                            desc_hl = "String",
-                            key = "l",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = "Lazy",
-                        },
-                        {
-                            icon = " ",
-                            icon_hl = "Title",
-                            desc = "Sync",
-                            desc_hl = "String",
-                            key = "u",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = "Lazy sync",
-                        },
-                        {
-                            icon = "󰢛 ",
-                            icon_hl = "Title",
-                            desc = "Mason",
-                            desc_hl = "String",
-                            key = "m",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = "Mason",
-                        },
-                        {
-                            icon = " ",
-                            icon_hl = "Title",
-                            desc = "Git",
-                            desc_hl = "String",
-                            key = "gs",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = "lua require('neogit').open()",
-                        },
-                        {
-                            icon = "󰗼 ",
-                            icon_hl = "Title",
-                            desc = "Exit",
-                            desc_hl = "String",
-                            key = "q",
-                            key_hl = "Number",
-                            -- key_format = " %s", -- remove default surrounding `[]`
-                            action = "q",
-                        },
+                center = {
+                    {
+                        icon = "󰦛 ",
+                        icon_hl = "Title",
+                        desc = "Restore Session",
+                        desc_hl = "String",
+                        key = "ss",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = "lua require('persistence').load()",
+                    },
+                    {
+                        icon = " ",
+                        icon_hl = "Title",
+                        desc = "New File",
+                        desc_hl = "String",
+                        key = "n",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = ":silent enew",
+                    },
+                    {
+                        icon = "󰂠 ",
+                        icon_hl = "Title",
+                        desc = "Lazy",
+                        desc_hl = "String",
+                        key = "l",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = "Lazy",
+                    },
+                    {
+                        icon = " ",
+                        icon_hl = "Title",
+                        desc = "Sync",
+                        desc_hl = "String",
+                        key = "u",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = "Lazy sync",
+                    },
+                    {
+                        icon = "󰢛 ",
+                        icon_hl = "Title",
+                        desc = "Mason",
+                        desc_hl = "String",
+                        key = "m",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = "Mason",
+                    },
+                    {
+                        icon = " ",
+                        icon_hl = "Title",
+                        desc = "Git",
+                        desc_hl = "String",
+                        key = "gs",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = "lua require('neogit').open()",
+                    },
+                    {
+                        icon = "󰗼 ",
+                        icon_hl = "Title",
+                        desc = "Exit",
+                        desc_hl = "String",
+                        key = "q",
+                        key_hl = "Number",
+                        -- key_format = " %s", -- remove default surrounding `[]`
+                        action = "q",
                     },
                 },
-            }
-        end, 2000)
+            },
+        }
     end,
 }
