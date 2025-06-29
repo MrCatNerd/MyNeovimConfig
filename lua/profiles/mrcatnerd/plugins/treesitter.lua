5d205ba6f50fd8cc1a57826c525a86c89edc9c21
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-06-29 16:00:52 +0300

    stuff

--- lua/profiles/mrcatnerd/plugins/treesitter.lua
+++ lua/profiles/mrcatnerd/plugins/treesitter.lua
@@ -83,7 +83,7 @@ return {
             disable = function(lang, buf)
                 local max_filesize = 100 * 1024 -- 100 KB
                 local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then return true end
+                return ok and stats and stats.size > max_filesize
             end,
         },
     },
