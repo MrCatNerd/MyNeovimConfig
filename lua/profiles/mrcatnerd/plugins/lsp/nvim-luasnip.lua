703b7128240a5428fa7b1acb4388eccbd6f08107
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-11-15 01:32:57 +0200

    replaced tag with version in luasnip config
    prbly got confused with packer

--- lua/profiles/mrcatnerd/plugins/lsp/nvim-luasnip.lua
+++ lua/profiles/mrcatnerd/plugins/lsp/nvim-luasnip.lua
@@ -1,6 +1,6 @@
 return {
     "L3MON4D3/LuaSnip",
-    tag = "v2.*",
+    version = "v2.*",
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
     dependencies = "rafamadriz/friendly-snippets",
