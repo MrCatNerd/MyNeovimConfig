32f04ea94f049aa97a968adf1add278e75897f22
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-11-22 23:04:45 +0200

    Changed some DAP config stuff

--- lua/profiles/mrcatnerd/plugins/dap/dap-ui.lua
+++ lua/profiles/mrcatnerd/plugins/dap/dap-ui.lua
@@ -8,15 +8,5 @@ return {
             { desc = "DAP UI toggle ui" },
         },
     },
-
-    config = function(_, opts)
-        require("dapui").setup(opts)
-
-        local dap, dapui = require "dap", require "dapui"
-
-        dap.listeners.before.attach.dapui_config = function() dapui.open() end
-        dap.listeners.before.launch.dapui_config = function() dapui.open() end
-        dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-        dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
-    end,
+    config = function(_, opts) require("dapui").setup(opts) end,
 }
--- lua/profiles/mrcatnerd/plugins/dap/dap.lua
+++ lua/profiles/mrcatnerd/plugins/dap/dap.lua
@@ -45,7 +45,13 @@ return {
         },
     },
     config = function()
-        require "dap"
+        local dap, dapui = require "dap", require "dapui"
+
+        -- launch dapui automatically when dapping
+        dap.listeners.before.attach.dapui_config = function() dapui.open() end
+        dap.listeners.before.launch.dapui_config = function() dapui.open() end
+        dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+        dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
 
         -- signs and fancy colors
         vim.fn.sign_define("DapBreakpoint", {
@@ -83,5 +89,29 @@ return {
         vim.api.nvim_set_hl(0, "DapLogPointLine", {})
         vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
         vim.api.nvim_set_hl(0, "DapStoppedLine", {})
+
+        dap.adapters.codelldb = {
+            type = "executable",
+            command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+            -- On windows you may have to uncomment this:
+            -- detached = false,
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch file",
+                type = "codelldb",
+                request = "launch",
+                program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+                args = {
+                    function() return vim.fn.input "Args: " end,
+                },
+            },
+        }
+        dap.configurations.c = dap.configurations.cpp
+        dap.configurations.rust = dap.configurations.cpp
     end,
 }
